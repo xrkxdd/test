f2ab0ad300ecde7317cf558fe3ce81fdd8b4a845
turb0 <nazar.mc28@gmail.com>
2024-11-11 13:17:53 +0100

    tr4

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HomeView  # Ensure this import is correct
 
 urlpatterns = [
-    path('home', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
 ]
