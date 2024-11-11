167dc55b051799ff365005b23f1a39ae1aba503f
turb0 <nazar.mc28@gmail.com>
2024-11-11 12:59:20 +0100

    tr2

--- testservice/asgi.py
+++ testservice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testservice project.
+ASGI settings for testservice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- testservice/wsgi.py
+++ testservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for testservice project.
+WSGI settings for testservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
