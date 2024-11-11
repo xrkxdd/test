f5d4fe920e86abde7fe73c9681ccc87ff998a0e6
turb0 <nazar.mc28@gmail.com>
2024-11-11 13:24:18 +0100

    tr5

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+from . import admin
 from .views import HomeView  # Ensure this import is correct
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('admin/', admin.site.urls),
 ]
