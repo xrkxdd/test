cdd29c2936104c2b638511b0a8c98f8e4ef5763b
turb0 <nazar.mc28@gmail.com>
2024-11-11 12:55:44 +0100

    tr

--- config/base.py
+++ config/base.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
