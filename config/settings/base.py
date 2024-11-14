ac7384b503d2c30485ea8e5fbd83c9e8bd8b4391
turb0 <nazar.mc28@gmail.com>
2024-11-14 02:10:04 +0100

    ts2

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
