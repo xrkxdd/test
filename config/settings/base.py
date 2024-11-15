d3de0b9ea7a4e4fa6491265240881edf02ff9a64
turb0 <nazar.mc28@gmail.com>
2024-11-15 18:31:17 +0100

    awedawdawdawd

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'testservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
