91bb91294d277ff9e097980bf54d8eb2159601ed
turb0 <nazar.mc28@gmail.com>
2024-11-15 18:14:29 +0100

    awedawd

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
